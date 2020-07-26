0d0e182ab7ee55ba044e056dbe9402c0eac88885
Elizabeth Mcmillan <ermcmillan@crimson.ua.edu>
2020-07-26 16:55:10 -0500

    Delete privacy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,6 @@ namespace RewriteYourSummer.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         public IActionResult Blog()
         {
